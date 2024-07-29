8ba1be1fec64c900cf03ceb44b518824617b5e42
sans2505 <131708440+sans2505@users.noreply.github.com>
2024-07-29 16:50:47 +0530

    Delete CommandPatternExample directory

--- CommandPatternExample/Light.java
+++
@@ -1,9 +0,0 @@
-public class Light {
-    public void on() {
-        System.out.println("The light is on.");
-    }
-
-    public void off() {
-        System.out.println("The light is off.");
-    }
-}
