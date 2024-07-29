6c477678d3e030e57bf6d15130ed267142e778d3
sans2505 <131708440+sans2505@users.noreply.github.com>
2024-07-29 16:51:16 +0530

    Delete DependencyInjectionExample directory

--- DependencyInjectionExample/CustomerRepositoryImpl.java
+++
@@ -1,7 +0,0 @@
-public class CustomerRepositoryImpl implements CustomerRepository {
-    @Override
-    public Customer findCustomerById(String id) {
-        // For demonstration purposes, returning a dummy customer
-        return new Customer(id, "Sanskruti Sharma", "sans.sh@example.com");
-    }
-}
